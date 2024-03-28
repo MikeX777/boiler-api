d9041464fc53b2a129d7b498f7b0294693944cb0
mcuomo <michael.r.cuomo@gmail.com>
2024-03-27 20:43:06 -0400

    Initial Setup

---
+++ BoilerApi/WeatherForecast.cs
@@ -0,0 +1,15 @@
+namespace BoilerApi
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
