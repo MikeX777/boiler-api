e92d7fba04348052a99bac328dd45f1548e7900d
mcuomo <michael.r.cuomo@gmail.com>
2024-03-28 10:00:34 -0400

    Add Dummy Controller, Correct Versioning

--- BoilerApi/Program.cs
+++ BoilerApi/Program.cs
@@ -66,10 +66,12 @@ builder.Services.AddApiVersioning(
         options.DefaultApiVersion = new ApiVersion(1, 0);
         options.AssumeDefaultVersionWhenUnspecified = true;
         options.ReportApiVersions = true;
+    })
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
     });
-
-// TODO: Need to test this
-builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddCors(options =>
 {
